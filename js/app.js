7dd46483176ad7a1f2c5d33ed299c3bcec4e7284
Resume Website <jdcollins242@gmail.com>
2021-05-09 08:04:22 -0400

    Fixing images

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
 let homeHeader = document.getElementById("home-header");
-let exploreHeader = document.getElementById("explore-header");
 
 // Get hours of the day in 24Hr format (0-23)
 let hr = new Date().getHours();
 
 const setBg = (headerName) => {
-    let dayBg = "url('./img/header-bg.png')"
-    let nightBg = "url('./img/night-bg.jpg')"
-    let setBg = '';
+    let dayBg = "url('./img/header-bg.png')";
+    let nightBg = "url('./img/night-bg.jpg')";
+    let setBg = "";
 
     if (hr >= 20 || hr <= 5) {
         setBg = nightBg;
     } else {
         setBg = dayBg;
     }
-
-    headerName.style.backgroundImage = setBg
+    headerName.style.backgroundImage = setBg;
 };
 
 setBg(homeHeader);
